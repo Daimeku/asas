e45ca856c4e59f43b4fb77bc038633c7602bac21
Drew92 <amagayle@gmail.com>
2015-05-06 13:05:56 -0500

    admin and invigilator relations made

--- database/seeds/UserCoursesSeeder.php
+++ database/seeds/UserCoursesSeeder.php
@@ -1948,5 +1948,272 @@ Class UserCoursesSeeder extends Seeder {
 			]
 		);
 
+		//Student Services-------
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>8,
+			 	'occurence_id' =>1 
+			 	
+			]
+		);
+
+		
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>8,
+			 	'occurence_id' =>2
+			 	
+			]
+		);
+
+		
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>8,
+			 	'occurence_id' =>3 
+			 	
+			]
+		);
+
+		
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>8,
+			 	'occurence_id' =>4
+			 	
+			]
+		);
+		
+		
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>8,
+			 	'occurence_id' =>5 
+			 	
+			]
+		);
+
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>8,
+			 	'occurence_id' =>6 
+			 	
+			]
+		);
+
+		
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>8,
+			 	'occurence_id' =>7
+			 	
+			]
+		);
+
+		
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>8,
+			 	'occurence_id' =>8 
+			 	
+			]
+		);
+
+		
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>8,
+			 	'occurence_id' =>9
+			 	
+			]
+		);
+		
+		
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>8,
+			 	'occurence_id' =>10 
+			 	
+			]
+		);
+
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>8,
+			 	'occurence_id' =>11
+			 	
+			]
+		);
+
+		
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>8,
+			 	'occurence_id' =>12
+			 	
+			]
+		);
+
+		
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>8,
+			 	'occurence_id' =>13 
+			 	
+			]
+		);
+
+		
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>8,
+			 	'occurence_id' =>14
+			 	
+			]
+		);
+		
+		
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>8,
+			 	'occurence_id' =>15 
+			 	
+			]
+		);
+
+		//-Invigilator-----
+
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>9,
+			 	'occurence_id' =>1 
+			 	
+			]
+		);
+
+		
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>9,
+			 	'occurence_id' =>2
+			 	
+			]
+		);
+
+		
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>9,
+			 	'occurence_id' =>3 
+			 	
+			]
+		);
+
+		
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>9,
+			 	'occurence_id' =>4
+			 	
+			]
+		);
+		
+		
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>9,
+			 	'occurence_id' =>5 
+			 	
+			]
+		);
+
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>9,
+			 	'occurence_id' =>6 
+			 	
+			]
+		);
+
+		
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>9,
+			 	'occurence_id' =>7
+			 	
+			]
+		);
+
+		
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>9,
+			 	'occurence_id' =>8 
+			 	
+			]
+		);
+
+		
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>9,
+			 	'occurence_id' =>9
+			 	
+			]
+		);
+		
+		
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>9,
+			 	'occurence_id' =>10 
+			 	
+			]
+		);
+
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>9,
+			 	'occurence_id' =>11 
+			 	
+			]
+		);
+
+		
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>9,
+			 	'occurence_id' =>12
+			 	
+			]
+		);
+
+		
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>9,
+			 	'occurence_id' =>13 
+			 	
+			]
+		);
+
+		
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>9,
+			 	'occurence_id' =>14
+			 	
+			]
+		);
+		
+		
+		DB::table('user_courses')->insert(
+			[
+			 	'user_id' =>9,
+			 	'occurence_id' =>15 
+			 	
+			]
+		);
+
 	}
 }
